042d2046dcdb39df89342a4a70ec2569e1e42fdf
raymond zhao <raymondzhao158@gmail.com>
2021-04-05 13:43:13 -0700

    commit tree 19b76017ee0d5d0244f40d1e9e077e013f5cd5ae

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const hqBlock = 42
+
+function distanceFromHqInBlocks(targetBlock) {
+    return Math.abs(hqBlock - targetBlock)
+};
+
+
+function distanceFromHqInFeet(blockEnded) {
+    return (Math.abs(hqBlock - blockEnded)*264)
+};
+
+function distanceTravelledInFeet(blockStarted, blockEnded) {
+    return (Math.abs(blockStarted - blockEnded)*264)
+};
+
+function calculatesFarePrice(start, destination) {
+    let fee;
+    if (distanceTravelledInFeet(start, destination) <400) {
+        fee = 0
+    }
+    if ((distanceTravelledInFeet(start, destination) >=400) && (distanceTravelledInFeet(start, destination) <=2000)) {
+        fee = (distanceTravelledInFeet(start, destination)-400)*.02
+    }
+    if ((distanceTravelledInFeet(start, destination) >2000) && (distanceTravelledInFeet(start, destination) <=2500)) {
+        fee = 25
+    }
+    if (distanceTravelledInFeet(start, destination) >2500) {
+        fee = `cannot travel that far`
+    }
+    return fee
+};
